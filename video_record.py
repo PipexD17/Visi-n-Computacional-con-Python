c2a8a67fd389f101d7209b97cf5f6a085391f0a9
PipexD17 <pipereira1234567z@gmail.com>
2020-12-11 11:52:09 -0400

    Se graba Video

--- video_record.py
+++ video_record.py
@@ -7,13 +7,15 @@ import cv2
 
 # Adquirimos la imagen de la web-cam
 captura = cv2.VideoCapture(0)
-
+salida = cv2.VideoWriter('video_salida.avi', cv2.VideoWriter_fourcc(*'XVID'), 30.0, (640, 480))
 while(captura.isOpened()):
     ret, img = captura.read()
     if ret == True:
         cv2.imshow('Video', img)
+        salida.write(img)
         if cv2.waitKey(1) and 0xFF == ord('s'):
             break
 
 captura.release()
+salida.release()
 cv2.destroyAllWindows()
